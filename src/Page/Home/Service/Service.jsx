ab192a9d7d564c5743b071b46984a3cd98bc853c
Sohelrana2815 <sohel152302@gmail.com>
2024-08-27 12:38:01 +0600

    first commit

---
+++ src/Page/Home/Service/Service.jsx
@@ -0,0 +1,16 @@
+const Service = () => {
+  return (
+    <div className="text-center">
+      <h2 className=" text-red-600">Service</h2>
+      <h3>Our Service Area</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui at quisquam
+        eos harum repellat recusandae est nemo, magnam ipsa nostrum earum quia
+        accusantium vitae sapiente deleniti consequuntur pariatur asperiores
+        necessitatibus?
+      </p>
+    </div>
+  );
+};
+
+export default Service;
